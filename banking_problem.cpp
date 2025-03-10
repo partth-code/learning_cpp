c500a651fe505a2f50fa415ccc56d3b8cfa469fd
Parth Yadav <parthcompiler@gmail.com>
2025-03-11 00:55:45 +0530

    project_commit

--- banking_problem.cpp
+++ banking_problem.cpp
@@ -2,32 +2,87 @@
 
 float total_amount = 10;
 
-void deposit(float amount);
-void  withdraw(float amount);
-void balance();
+void deposit(double balance);
+void  withdraw(double balance);
+void ac_balance(double balance);
 
 int main()
 {
-    float money = 23.5;
+    double balance = 0;
+    int choice = 0;
 
-    deposit(43);
-    withdraw(12);
-    balance();
+    do{
+
+    std::cout<<"\nEnter\n1. To check balance\n2. To deposit money\n3. To withdraw money\n4. To exit\n";
+    std::cout<<"Enter choice: ";
+    std::cin>>choice;
+
+    
+        switch(choice)
+        {
+            case 1:
+                ac_balance(balance);
+                break;
+            case 2:
+                deposit(balance);
+                break;
+            case 3:
+                withdraw(balance);
+                break;
+            case 4:
+                std::cout<<"Thanks for visiting!!"<<'\n';
+                break;
+            default:
+                std::cout<<"Invalied Input\n";
+                break;
+        }
+    }while(choice != 4);
 
     return 0;
 }
 
-void deposit(float amount)
+void deposit(double balance)
 {
-     total_amount+=amount;
+    double amount;
+     std::cout<<"Enter amount you want to deposit, in $";
+     std::cin>>amount;
+
+     if (amount>0)
+     {
+        std::cout<<"\nYou deposited $"<<amount;
+
+        balance = balance+amount;
+        std::cout<<"\nBalance in you accout is $"<<balance;
+     }
+     else
+     {
+        std::cout<<"\nYou entered an invalied amount";
+
+     }
+
 }
 
-void withdraw(float amount)
+void withdraw(double balance)
 {
-    total_amount -=amount;
+    double amount;
+    std::cout<<"\nEnter amount you want to withdraw, in $";
+    std::cin>>amount;
+
+    if(amount>balance)
+    {
+        std::cout<<"\nNot enough balance to withdraw $"<<amount;
+       
+    }
+    else
+    {
+        std::cout<<"\nYou have withdrawin $"<<amount;
+        
+        balance = balance-amount;
+        std::cout<<"\nBalance in you accout is $"<<balance;
+    }
 }
 
-void balance()
+void ac_balance(double balance)
 {
-    std::cout<<"Totoal balance of your account is: "<<total_amount;
+    std::cout<<"\nTotal balance of your account is $"<<balance;
 }
--- rock_papaer_sissors_game.cpp
+++ rock_papaer_sissors_game.cpp
@@ -1,16 +1,33 @@
 #include<iostream>
+#include<ctime>
+#include<cstdlib>
 
 int main()
 {
-    std::string fruits[] = {"apple","mango","banana","pineapple"};
+     std::srand(std::time(NULL));
 
-    //or we can decleare an array wit hsize specified ,then use assign it later
+     int comp_choice = (std::rand() %3)+1;
+     
+     int player_choice;
+     
+     std::cout<<"Enter\n 1 for stone\n 2 for paper\n 3 3 for sissors";
+     std::cout<<"\nEnter choice: ";
+     std::cin>>player_choice;
 
-    std::string fruit[3];
+     std::cout<<"Choice of player: "<<player_choice<<"\nChoice od computer: "<<comp_choice<<'\n';
 
-    fruit[1] = "apple";
-
-    std::cout<<fruit[1];
+     if( (comp_choice == 1 && player_choice == 2) || (comp_choice == 2 && player_choice == 3) || (comp_choice == 3 && player_choice == 1) )
+     {
+        std::cout<<"Player Won!!";
+     }
+     else if(comp_choice==player_choice)
+     {
+        std::cout<<"There is a Tie";
+     }
+     else
+     {
+        std::cout<<"Computer Won!!";
+     }
 
     return 0;
 }
